1362140c8db4f44fd25ec51e3b237374475073e9
Tim Durden <tim.durden@surevine.com>
2015-04-15 11:25:45 +0100

    Testing env vars within yml.

--- features/support/pages/login_page.rb
+++ features/support/pages/login_page.rb
@@ -3,11 +3,13 @@ class LoginPage
   include FigNewton
 
   page_url("#{FigNewton.base_url}/login.jspa")
-  
+
   text_field(:username, :name => "username")
   text_field(:password, :id => "password01")
 
   def log_in(username = FigNewton.user1_uname, password = FigNewton.user1_pswd)
+    puts username
+    puts password
     self.username = username
     self.password = password
     @browser.button(:id => "login-submit").when_present.click
