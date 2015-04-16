d7fee00cd822477cde3e66d25049cf3ee3f56543
Mark Winteringham <mark.winteringham@surevine.com>
2015-04-16 15:39:57 +0100

    Removed more tmp reporting stuff

--- reporting/ci_email/process_email.rb
+++
@@ -1,7 +0,0 @@
-require 'premailer'
-
-premailer = Premailer.new('latest_run.html', :warn_level => Premailer::Warnings::SAFE)
-
-email = File.open("email.html", "w")
-email.puts premailer.to_inline_css
-email.close
