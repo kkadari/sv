6c7723e73688e2ba76b71ae2e08859d218fede90
Mark Winteringham <mark.winteringham@surevine.com>
2015-04-20 10:00:49 +0100

    Updated juc07 to pass with new API call

--- features/step_definitions/juc07_delete_an_incident_steps.rb
+++ features/step_definitions/juc07_delete_an_incident_steps.rb
@@ -1,6 +1,5 @@
 Then /^I can delete the incident report$/ do
-  on(HomePage).click_content
-  on(ContentPage).navigate_to_ir_named(@subject)
+  on(HomePage).navigate_to_ir_named(@subject)
   on(IncidentReportSummaryPage).verify_content_exists(@subject)
   on(IncidentReportSummaryPage).delete_incident_report
 end
