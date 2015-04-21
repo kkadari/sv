7b01787584821e1f7fbdc790f9d97059f1a758ce
Mark Winteringham <mark.winteringham@surevine.com>
2015-04-21 12:06:59 +0100

    Test fixes

--- features/step_definitions/juc04_comment_on_an_incident_steps.rb
+++ features/step_definitions/juc04_comment_on_an_incident_steps.rb
@@ -1,6 +1,6 @@
 Then /^I? (?:can|have)? (?:comment|commented) on the incident report( anonymously)?$/ do |anonymous|
   on(HomePage).click_content
-  on(HomePage).navigate_to_ir_named(@subject)
+  on(ContentPage).navigate_to_ir_named(@subject)
   if (anonymous)
     on(ViewIncidentReportPage).add_comment_anonymously
   else
@@ -10,7 +10,7 @@ end
 
 Then /^I? (?:can|have)? (?:review|reviewed) the incident report( anonymously)?$/ do |anonymous|
   on(HomePage).click_content
-  on(HomePage).navigate_to_ir_named(@subject)
+  on(ContentPage).navigate_to_ir_named(@subject)
   if (anonymous)
     on(ViewIncidentReportPage).add_review_anonymously
   else
@@ -26,7 +26,7 @@ Given /^I have mentioned "([^\"]+)" in an anonymous comment$/ do |user|
       when user_to_mention = 'participant A'
         on(ViewIncidentReportPage).add_anon_comment_mentioning_matt
       when user_to_mention = 'participant B'
-        on(ViewIncidentReportPage).add_anon_comment_mentioning_tim
+        on(ViewIncidentReportPage).add_anon_comment_mentioning_simonwi
       else
         fail 'Supplied user not recognised.'
     end
