9c7440c60fe2d6f88d68f02335c3c941d94d7b6a
Mark Winteringham <mark.winteringham@surevine.com>
2015-05-05 17:28:46 +0100

    Update to selectors to be compatible with Phantom

--- features/support/browser_factory.rb
+++ features/support/browser_factory.rb
@@ -21,7 +21,7 @@ module BrowserFactory
         driver = Watir::Browser.new :remote, url: 'http://localhost:8001'
       else # Not specified? Use Firefox.
         puts 'ENV[\'Browser\'] wasn\'t set.  Reverting to Firefox.'
-        profile = Selenium::WebDriver::Remote::Profile.new
+        profile = Selenium::WebDriver::Firefox::Profile.new
         profile['browser.helperApps.neverAsk.saveToDisk'] = "text/csv,application/pdf"
         driver = Watir::Browser.new :firefox, :profile => profile
     end
