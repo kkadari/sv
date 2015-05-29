81133b76d65f859416f020067ccfee0049f670b3
Mark Winteringham <mark.winteringham@surevine.com>
2015-05-29 11:57:28 +0100

    Parameterised int test auth

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,6 @@ require 'base64'
 
 RSpec.configure do |config|
   config.before(:all) do
-    @authorisation = 'Basic ' + Base64.encode64('markw@surevine:Grg@!6KsS1EY7Bb')
+    @authorisation = 'Basic ' + Base64.encode64(ENV['username'] + ':' + ENV['password'])
   end
 end
