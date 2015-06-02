4c20cb7044995fd38fd41082b289ce8a6093e5f5
Mark Winteringham <mark.winteringham@surevine.com>
2015-06-02 10:16:22 +0100

    Improved waits in JUC08

--- features/step_definitions/juc08_comment_on_a_discussion_steps.rb
+++ features/step_definitions/juc08_comment_on_a_discussion_steps.rb
@@ -8,8 +8,11 @@ Then /^I? (?:can|have)? (?:comment|commented) on the discussion( anonymously)?$/
     end
   end
 
-  fail 'Content not visible or created' unless @browser.html.to_s.include? @subject
   on ViewDiscussionPage do |comment|
+    comment.wait_until do
+      comment.title?
+    end
+
     comment.reply
     comment.enable_html_mode
     comment.comment_body = 'A discussion comment'
@@ -28,8 +31,11 @@ Then /^I can comment on the comment( anonymously)?$/ do |anonymous|
     end
   end
 
-  fail 'Content not visible or created' unless @browser.html.to_s.include? @subject
   on ViewDiscussionPage do |comment|
+    comment.wait_until do
+      comment.title?
+    end
+
     comment.reply
     comment.enable_html_mode
     comment.comment_body = 'Anonymous comment for discussion'
