b3d170d9e553c72699b427ce14f9aa7376c9886c
Mark Winteringham <mark.winteringham@surevine.com>
2015-06-02 14:10:27 +0100

    Disabling HAR file export due to disk space issues

--- features/support/browser_factory.rb
+++ features/support/browser_factory.rb
@@ -5,31 +5,31 @@ module BrowserFactory
 
     case ENV['browser']
       when 'firefox'
-        tmp_folder = File.dirname(__FILE__) + '/../../reporting/tmp'
-        FileUtils.rm_rf(tmp_folder) if File.exists?(tmp_folder)
-        Dir.mkdir(tmp_folder) unless File.exists?(tmp_folder)
-
-        firebug_path = File.dirname(__FILE__) + '/../../bin/firebug-2.0.9-fx.xpi'
-        net_export_path = File.dirname(__FILE__) + '/../../bin/netExport-0.9b7.xpi'
-
-        profile = Selenium::WebDriver::Firefox::Profile.new
-        profile.add_extension(firebug_path)
-        profile.add_extension(net_export_path)
-
-        profile['extensions.firebug.allPagesActivation']  = 'on'
-        profile['extensions.firebug.currentVersion'] = '2.0.9'
-        profile['extensions.firebug.DBG_NETEXPORT'] = false
-        profile['extensions.firebug.onByDefault'] = true
-        profile['extensions.firebug.defaultPanelName'] = 'net'
-        profile['extensions.firebug.net.enableSites'] = true
-        profile['extensions.firebug.net.defaultPersist'] = true
-        profile['extensions.firebug.netexport.alwaysEnableAutoExport'] = true
-        profile['extensions.firebug.netexport.autoExportToFile'] = true
-        profile['extensions.firebug.netexport.autoExportToServer'] = false
-        profile['extensions.firebug.netexport.defaultLogDir'] = File.dirname(__FILE__) + '/../../reporting/tmp'
-        profile['extensions.firebug.netexport.showPreview'] = true
-        profile['extensions.firebug.netexport.sendToConfirmation'] = false
-        profile['extensions.firebug.netexport.Automation'] = true
+        # tmp_folder = File.dirname(__FILE__) + '/../../reporting/tmp'
+        # FileUtils.rm_rf(tmp_folder) if File.exists?(tmp_folder)
+        # Dir.mkdir(tmp_folder) unless File.exists?(tmp_folder)
+        #
+        # firebug_path = File.dirname(__FILE__) + '/../../bin/firebug-2.0.9-fx.xpi'
+        # net_export_path = File.dirname(__FILE__) + '/../../bin/netExport-0.9b7.xpi'
+        #
+        # profile = Selenium::WebDriver::Firefox::Profile.new
+        # profile.add_extension(firebug_path)
+        # profile.add_extension(net_export_path)
+        #
+        # profile['extensions.firebug.allPagesActivation']  = 'on'
+        # profile['extensions.firebug.currentVersion'] = '2.0.9'
+        # profile['extensions.firebug.DBG_NETEXPORT'] = false
+        # profile['extensions.firebug.onByDefault'] = true
+        # profile['extensions.firebug.defaultPanelName'] = 'net'
+        # profile['extensions.firebug.net.enableSites'] = true
+        # profile['extensions.firebug.net.defaultPersist'] = true
+        # profile['extensions.firebug.netexport.alwaysEnableAutoExport'] = true
+        # profile['extensions.firebug.netexport.autoExportToFile'] = true
+        # profile['extensions.firebug.netexport.autoExportToServer'] = false
+        # profile['extensions.firebug.netexport.defaultLogDir'] = File.dirname(__FILE__) + '/../../reporting/tmp'
+        # profile['extensions.firebug.netexport.showPreview'] = true
+        # profile['extensions.firebug.netexport.sendToConfirmation'] = false
+        # profile['extensions.firebug.netexport.Automation'] = true
         profile['browser.helperApps.neverAsk.saveToDisk'] = 'text/csv,application/pdf'
         driver = Watir::Browser.new :firefox, :profile => profile
 
