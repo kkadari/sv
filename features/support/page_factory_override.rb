d86926705e5ef92f4edbf1b5c2105ced00a65612
Mark Winteringham <mark.winteringham@surevine.com>
2015-06-03 15:01:26 +0100

    Added in handling to benchmark for phantom

--- features/support/page_factory_override.rb
+++ features/support/page_factory_override.rb
@@ -5,7 +5,10 @@ module PageFactoryOverride
 
   def visit_and_benchmark(page_class, params={:using_params => {}}, &block)
     visit(page_class, params, &block)
-    load_secs = @browser.performance.summary[:response_time]/1000
-    puts "Page load time: #{load_secs} seconds."
+
+    begin
+      load_secs = @browser.performance.summary[:response_time]/1000
+      puts "Page load time: #{load_secs} seconds."
+    end
   end
 end
