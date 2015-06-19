9dab6d65e44f725b9f97e793264a5d086acda890
Mark Winteringham <mark.winteringham@surevine.com>
2015-06-19 12:05:52 +0100

    Places page updates

--- spec/jive_page/places_page_spec.rb
+++ spec/jive_page/places_page_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
-describe 'Places page' do
+describe 'Places view page' do
 
-  it 'should return a 200 when requested' do
+  it 'should return a 200 when requesting the place page' do
     RestClient.get(ENV['base_url'] + '/places',:cookie => @authorisation){|response|
+      fail('Failed with ' + response.code.to_s) if response.code != 200
+    }
+  end
+
+  it 'should return a 200 when requesting one of the filtered views of places' do
+    RestClient.get(ENV['base_url'] + '/__services/v2/rest/containers?filterGroupID=places&itemViewID=thumb&start=0&numResults=20&filterID=historyarchetype%5Bcontainers%5D&itemView=thumb&userID=2012',:cookie => @authorisation){|response|
       fail('Failed with ' + response.code.to_s) if response.code != 200
     }
   end
