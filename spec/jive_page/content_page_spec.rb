275663ca1cf27b45d1f0c8c987603d9b551c56e6
Mark Winteringham <mark.winteringham@surevine.com>
2015-06-22 10:56:53 +0100

    Content widget spec

--- spec/jive_page/content_page_spec.rb
+++ spec/jive_page/content_page_spec.rb
@@ -8,10 +8,4 @@ describe 'Content page' do
     }
   end
 
-  it 'should return a 200 when requesting one of the filtered views of content' do
-    RestClient.get(ENV['base_url'] + '/__services/v2/rest/content?filterGroupID=content&token=MTQzNDcxOTE1MTE3MnwyMHxbQkAxMWIzNjQxOQ%3D%3D&itemViewID=detail&start=0&numResults=20&filterID=historyarchetype%5Bcontent%5D&itemView=detail&userID=2012',:cookie => @authorisation){|response|
-      fail('Failed with ' + response.code.to_s) if response.code != 200
-    }
-  end
-
 end
