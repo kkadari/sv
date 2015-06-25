888a4098d564abb283571c01cadbfd3a694fc29e
Mark Winteringham <mark.winteringham@surevine.com>
2015-06-25 14:48:46 +0100

    Test fixes - removed poll test due to Jive poll option instability

--- spec/jive_page/create_poll_page_spec.rb
+++ spec/jive_page/create_poll_page_spec.rb
@@ -18,20 +18,6 @@ describe 'Create poll page' do
     }
   end
 
-  it 'should return a 200 when requesting to move a poll choice' do
-    RestClient.get('http://dev188.sure.vine/poll/create.jspa?sr=cmenu&containerType=2020&containerID=1024',:cookie => @authorisation){|response|
-      @poll_id = response.body.scan(/name="pollID" value="([0-9]*)/).join(',')
-    }
-
-    RestClient.post(ENV['base_url'] + '/__services/v2/rest/polls/' + @poll_id + '/pollOptions','',:cookie => @authorisation){|response|
-      @choice_id = (JSON.parse(response)['id'] - 1).to_s
-    }
-
-    RestClient.post(ENV['base_url'] + '/__services/v2/rest/polls/' + @poll_id + '/pollOptions/' + @choice_id.to_s + '/index','index=0',{:cookie => @authorisation,:content_type => 'application/x-www-form-urlencoded'}){|response|
-      fail('Failed with ' + response.code.to_s) if response.code != 200
-    }
-  end
-
   it 'should return a 200 when creating a poll' do
     payload = PollPayload
                   .new(@authorisation,
@@ -44,5 +30,4 @@ describe 'Create poll page' do
       fail('Failed with ' + response.code.to_s) if response.code != 200
     }
   end
-
 end
