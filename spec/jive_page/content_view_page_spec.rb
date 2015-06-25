fc02624fc0c2fcb9d7e32a67b8f57bed4f16f37e
Mark Winteringham <mark.winteringham@surevine.com>
2015-06-25 13:52:22 +0100

    Test fixes

--- spec/jive_page/content_view_page_spec.rb
+++ spec/jive_page/content_view_page_spec.rb
@@ -39,23 +39,6 @@ describe 'Viewing content' do
     }
   end
 
-  it 'should return a 200 when viewing a poll' do
-    payload = PollPayload
-                  .new(@authorisation,
-                       Faker::Lorem.sentence(3),
-                       Faker::Lorem.sentence(20),
-                       'random',
-                       Faker::Lorem.words(2).join(',')).payload
-
-    RestClient.post(ENV['base_url'] + '/__services/v2/rest/polls',payload,{:cookie => @authorisation,:content_type => 'application/json; charset=UTF-8'}){|response|
-      @payload = JSON.parse(response.body)
-    }
-
-    RestClient.get(ENV['base_url'] + '/' + @payload['redirect'],:cookie => @authorisation){|response|
-      fail('Failed with ' + response.code.to_s) if response.code != 200
-    }
-  end
-
   it 'should return 200 when viewing a Incident Report' do
     payload = IncidentReportPayload
                   .new(Faker::Lorem.sentence(3),
