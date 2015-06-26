b79a8a506ade24718bf2c6c8510bccb3ec05e0f9
Mark Winteringham <mark.winteringham@surevine.com>
2015-06-26 10:33:44 +0100

    More params for places and groups

--- spec/jive_page/profile_activity_page_spec.rb
+++ spec/jive_page/profile_activity_page_spec.rb
@@ -28,7 +28,7 @@ describe 'The profile activity page' do
   end
 
   it 'should return a 200 when requesting most liked' do
-    RestClient.get(ENV['base_url'] + '/most-liked.jspa?containerType=14&container=1&numResults=10&filterUserID=' + @ide,:cookie => @authorisation){|response|
+    RestClient.get(ENV['base_url'] + '/most-liked.jspa?containerType=14&container=1&numResults=10&filterUserID=' + @id,:cookie => @authorisation){|response|
       fail('Failed with ' + response.code.to_s) if response.code != 200
     }
   end
--- spec/jive_page/space_subspaces_page_spec.rb
+++ spec/jive_page/space_subspaces_page_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'Space subspaces' do
 
   it 'should return a 200 when requested' do
-    RestClient.get(ENV['base_url'] + '/community/test-space/places',{:cookie => @authorisation, :content_type => 'application/json'}){|response|
+    RestClient.get(ENV['base_url'] + '/community/' + ENV['space'] + '/places',{:cookie => @authorisation, :content_type => 'application/json'}){|response|
       fail('Failed with ' + response.code.to_s) if response.code != 200
     }
   end
