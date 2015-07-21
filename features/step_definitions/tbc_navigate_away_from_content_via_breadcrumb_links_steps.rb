293a48d2ff868500d2b85c5010a30e4e70e81197
Mark Winteringham <mark.winteringham@surevine.com>
2015-07-21 16:10:54 +0100

    Automated breadcrumb feature

--- features/step_definitions/tbc_navigate_away_from_content_via_breadcrumb_links_steps.rb
+++ features/step_definitions/tbc_navigate_away_from_content_via_breadcrumb_links_steps.rb
@@ -27,3 +27,31 @@ end
 Then /I am able to view more polls in a related container$/ do
   fail 'Not on correct content page' unless @browser.url.include? "/people/#{@test_config_set[:user_1_name].gsub('@','%40')}/content?filterID=contentstatus[published]~objecttype~objecttype[poll]"
 end
+
+Given /^I am viewing an incident report$/ do
+  switch_user('participant A')
+  subject = TitleCreator.create_title_for('incident')
+
+  response = CreateContent.create_incident_report @browser.cookies.to_a, subject, 'Lorem ipsumy goodness', 'white', Hash[:type => 'community'], "", false
+  incident_id = response['redirect'][/[0-9]+/,0]
+
+  @response = Content.get_ir(incident_id, @browser.cookies.to_a)
+end
+
+Then /^I am able to view more incident reports in a related container$/ do
+  fail('Related link not found') unless Nokogiri::HTML.parse(@response).css('.j-context > a')[0].to_s.include? '/content?filterID=contentstatus%5Bpublished%5D~objecttype~objecttype%5Bincidentreport%5D'
+end
+
+Given /^I am viewing a discussion$/ do
+  switch_user('participant A')
+  subject = TitleCreator.create_title_for('discussion')
+
+  response = CreateContent.create_discussion @browser.cookies.to_a, subject, false, 'Lorem ipsumy goodness', 'white', Hash[:type => 'community'], '', false
+  discussion_id = response['redirect'][/[0-9]+/,0]
+
+  @response = Content.get_message(discussion_id, @browser.cookies.to_a)
+end
+
+Then /^I am able to view more discussion in a related container$/ do
+  fail('Related link not found') unless Nokogiri::HTML.parse(@response).css('.j-context > a')[0].to_s.include? '/content?filterID=contentstatus%5Bpublished%5D~objecttype~objecttype%5Bthread%5D'
+end
