664a2ca4128c0687657920abcfd37fc4d788ceba
Mark Winteringham <mark.winteringham@surevine.com>
2015-07-30 16:07:07 +0100

    Migrated JUC12 to endpoint tests

--- features/step_definitions/juc12_raise_an_incident_steps.rb
+++ features/step_definitions/juc12_raise_an_incident_steps.rb
@@ -3,7 +3,7 @@ Given /^I have mentioned "([^\"]+)" in? (?:a|an) (red|amber|green|white) inciden
   @marking = marking
   @location = location
   user_profile = TestConfig.return_profile(user)
-  mention = 'Some sample text <p><a class="jive_macro jive_macro_user" href="javascript:;" jivemacro="user" ___default_attr="' + user_profile[:user_id] + '" data-objecttype="3" data-orig-content="' + user_profile[:username] + '">' + user_profile[:username] + '</a></p>'
+  mention = 'Some sample text <p><a class=\"jive_macro jive_macro_user\" href=\"javascript:;\" jivemacro=\"user\" ___default_attr=\"' + user_profile[:user_id] + '\" data-objecttype=\"3\" data-orig-content=\"' + user_profile[:username] + '\">' + user_profile[:username] + '</a></p>'
 
   payload = IncidentReportPayload.new(@subject, false, mention, @marking, {:type => @location}, '', anonymous).payload
   response = CreateContent.create_incident_report(payload, @browser.cookies.to_a)
@@ -23,24 +23,25 @@ Given /^I have raised a white incident report in a private group containing an i
 end
 
 Then /^I can view the internal link$/ do
-  visit_and_benchmark ViewIncidentReportPage, :using_params => {:id => @incident_id}
+  response = Content.get_ir(@incident_id, @browser.cookies.to_a)
+  link = Nokogiri::HTML.parse(response).css('.jive-rendered-content').text
 
-  fail 'Content not visible or created' unless @browser.html.to_s.include? @subject
-  fail 'Link not present' unless @browser.html.include? @incident_url
+  fail 'Link not present' unless link.include? @incident_url
 end
 
 Then /^I am able to view the marking$/ do
-  visit_and_benchmark ViewIncidentReportPage, :using_params => {:id => @incident_id}
+  response = Content.get_ir(@incident_id, @browser.cookies.to_a)
+  tlp = Nokogiri::HTML.parse(response).css('.ihmbar').text.downcase
 
-  fail 'Content not visible or created' unless @browser.html.to_s.include? @subject
-  on(IncidentReportSummaryPage).ihm_bar.downcase.include? @marking
+  fail 'Marking not present' unless tlp.include? @marking
 end
 
 Then /^I can verify the anonymous identifiers have been added$/ do
-  visit_and_benchmark ViewIncidentReportPage, :using_params => {:id => @incident_id}
+  response = Content.get_ir(@incident_id, @browser.cookies.to_a)
 
-  fail 'Content not visible or created' unless @browser.html.to_s.include? @subject
-  on(IncidentReportSummaryPage).ihm_bar.downcase.include? @marking
+  tlp = Nokogiri::HTML.parse(response).css('.ihmbar').text.downcase
+  fail 'Marking not present' unless tlp.include? @marking
 
-  fail 'Incident report not anonymous' unless @browser.html.to_s.include? 'This content was posted anonymously by its author'
+  anon = Nokogiri::HTML.parse(response).css('.anonymous-badge')[0]['title']
+  fail 'Incident report not anonymous' unless anon.include? 'This content was posted anonymously by its author'
 end
