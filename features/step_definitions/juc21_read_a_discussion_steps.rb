0cc1d501789af0fa00e5687b2820110a01992315
Mark Winteringham <mark.winteringham@surevine.com>
2015-07-30 11:11:41 +0100

    Migrated juc21 to endpoint usage

--- features/step_definitions/juc21_read_a_discussion_steps.rb
+++ features/step_definitions/juc21_read_a_discussion_steps.rb
@@ -1,19 +1,10 @@
 Then /^as an admin I can view the anonymous discussion$/ do
-  visit_and_benchmark DiscussionSummaryPage, :using_params => {:id => @discussion_id}
+  switch_user('admin')
 
-  fail 'Content not visible or created' unless @browser.html.to_s.include? @subject
-  fail 'Incident report not anonymous' unless @browser.html.to_s.include? 'This content was posted anonymously by its author'
-end
+  response = Content.get_message(@discussion_id, @browser.cookies.to_a)
 
-Then(/^I can view the discussion$/) do
-  on(DiscussionSummaryPage).wait_until do
-    on(DiscussionSummaryPage).title?
-  end
-
-  fail 'Title not visible' unless on(DiscussionSummaryPage).title?
-  fail 'IHM not visible' unless on(DiscussionSummaryPage).ihm_bar?
-  fail 'Body not visible' unless on(DiscussionSummaryPage).original_message?
-  fail 'Comment not visible' unless on(ViewDiscussionPage).reply?
+  title = Nokogiri::HTML.parse(response).css('.js-original-header > h1').text
+  fail 'Content not visible or created' unless title.include? @subject
 end
 
 When /^I attempt to view a discussion that has recently been deleted$/ do
@@ -29,4 +20,17 @@ Then /^the system displays a 'Not Found' error message$/ do
   switch_user('participant A')
 
   Content.get_message(@discussion_id, @browser.cookies.to_a, 404)
+end
+
+######## SMOKE TEST STEPS ########
+
+Then /^I can view the discussion$/ do
+  on(DiscussionSummaryPage).wait_until do
+    on(DiscussionSummaryPage).title?
+  end
+
+  fail 'Title not visible' unless on(DiscussionSummaryPage).title?
+  fail 'IHM not visible' unless on(DiscussionSummaryPage).ihm_bar?
+  fail 'Body not visible' unless on(DiscussionSummaryPage).original_message?
+  fail 'Comment not visible' unless on(ViewDiscussionPage).reply?
 end
