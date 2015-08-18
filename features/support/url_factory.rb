c9ae7c654871a847ec383137eeb24beabd73cf12
Mark Winteringham <mark.winteringham@surevine.com>
2015-08-18 16:37:13 +0100

    Removed un-used pages

--- features/support/url_factory.rb
+++ features/support/url_factory.rb
@@ -1,19 +1,7 @@
 module UrlFactory
-  
-  def loginpage
-    ENV['base_url'] + '/login.jspa'
-  end
-
-  def logoutpage
-    ENV['base_url'] + '/logout.jspa'
-  end
 
   def homepage
     ENV['base_url'] + '/'
-  end
-
-  def welcomepage
-    ENV['base_url'] + '/welcome'
   end
 
   def spacepage
@@ -32,36 +20,12 @@ module UrlFactory
     ENV['base_url'] + '/content'
   end
 
-  def blogposteditpage
-    ENV['base_url'] + "/blog/update-post#{/.*/}"
-  end
-
-  def blogpostpage
-    ENV['base_url'] + '/blog/create-post.jspa?sr=cmenu&containerType=14&containerID=1'
-  end
-
-  def blogpostpdfpage
-    /#{ENV['base_url']}\/people\S+.pdf/
-  end
-
   def blogpostsummarypage
     ENV['base_url'] + "/people/#{/.*/}"
   end
 
-  def discussioneditpage
-    ENV['base_url'] + "/message/#{/.*/}/edit"
-  end
-
   def viewuploadeddocumentpage
     ENV['base_url'] + "/docs/#{/.*/}"
-  end
-
-  def discussionpage
-    ENV['base_url'] + '/discussion/create.jspa?sr=cmenu&containerType=14&containerID=1'
-  end
-
-  def discussionpdfpage
-    /#{ENV['base_url']}\/thread\S\d+.pdf/
   end
 
   def discussionsummarypage
@@ -76,32 +40,8 @@ module UrlFactory
     ENV['base_url'] + '/groups/<%=params[:id]%>'
   end
 
-  def incidentreporteditpage
-    ENV['base_url'] + '/incidentreports/<%=params[:id]%>/edit'
-  end
-
-  def incidentreportpage
-    ENV['base_url'] + '/incidentreports/create-incidentreport.jspa!input?sr=cmenu&containerType=14&containerID=1'
-  end
-
-  def incidentreportpdfpage
-    /#{ENV['base_url']}\/incidentreports\S\d+.pdf/
-  end
-
   def incidentreportsummarypage
     /#{ENV['base_url']}\/view\-incidentreport\.jspa\?incidentReportID\=\d+/
-  end
-
-  def polleditpage
-    "#{ENV['base_url']}/poll/edit#{/.*/}"
-  end
-
-  def pollpage
-    ENV['base_url'] + '/poll/create.jspa?sr=cmenu&containerType=14&containerID=1'
-  end
-
-  def preferencespage
-    ENV['base_url'] + '/user-preferences!input.jspa'
   end
 
   def advsearchpage
@@ -112,32 +52,12 @@ module UrlFactory
     /#{ENV['base_url']}thread\/\d+/
   end
 
-  def viewincidentreportpage
-    ENV['base_url'] + '/incidentreports/<%=params[:id]%>'
-  end
-
-  def customgrouppage
-    ENV['base_url'] + '/groups/<%=params[:id]%>'
-  end
-
   def placespage
     ENV['base_url'] + '/places'
   end
 
   def pollsummarypage
     ENV['base_url'] + '/polls/<%=params[:id]%>'
-  end
-
-  def useroneactivitystreampage
-    ENV['base_url'] + '/people/<%=params[:id]%>/activity'
-  end
-
-  def useroneprivacyeditpage
-    ENV['base_url'] + '/edit-profile-security!input.jspa'
-  end
-
-  def useroneprofileeditpage
-    ENV['base_url'] + '/edit-profile!input.jspa'
   end
 
   def useroneprofilepage
