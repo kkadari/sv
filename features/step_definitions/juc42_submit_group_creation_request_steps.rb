221ec9f1df50abc6f036f9fe90d6cdf788357be5
Mark Winteringham <mark.winteringham@surevine.com>
2015-09-28 11:29:19 +0100

    Improving brittle group creation request

--- features/step_definitions/juc42_submit_group_creation_request_steps.rb
+++ features/step_definitions/juc42_submit_group_creation_request_steps.rb
@@ -72,30 +72,29 @@ Given /^I have requested a new group is created with participant B as an additio
 end
 
 Then /^I have received the group creation request in my inbox$/ do
-  visit_and_benchmark InboxPage do |inbox|
-    inbox.wait_until do
-      inbox.notifications?
+  5.times do |i|
+    begin
+      visit_and_benchmark InboxPage, :using_params => {:query => '?filter=notifications&et=extendedcontent.expertise.moderation&%3FobjectType=-1805099612'} do |inbox|
+        inbox.notifications
+
+        inbox.wait_until do
+          inbox.inbox_message?
+        end
+
+        fail('Notification not received') unless inbox.inbox_message_element.text.include? 'stephaniek@surevine added you as a potential second owner for a group named \'Auto request for group\'.'
+        fail('Notification description not correct') unless inbox.inbox_message_element.text.downcase.include? 'the group has the following description: description for the group'
+
+        inbox.confirm_notification
+      end
+      break
+    rescue => e
+      if i < 5
+        sleep(1)
+      else
+        fail(e)
+      end
     end
-
-    inbox.notifications
-
-    inbox.wait_until do
-      inbox.open_filter?
-    end
-
-    inbox.open_filter
-    inbox.filter_by_notificiation
-
-    inbox.wait_until do
-      inbox.inbox_message?
-    end
-
-    fail('Notification not received') unless inbox.inbox_message_element.text.include? 'stephaniek@surevine added you as a potential second owner for a group named \'Auto request for group\'.'
-    fail('Notification description not correct') unless inbox.inbox_message_element.text.downcase.include? 'the group has the following description: description for the group'
-
-    inbox.confirm_notification
   end
-
 end
 
 Given /^I have opted to request a new group$/ do
