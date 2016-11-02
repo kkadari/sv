66ad5479e9fbb0d3c266fb3269b52a6638b142c8
Tim Durden <tim.durden@surevine.com>
2016-11-02 12:55:24 +0000

    Changed hero banner URL to generic.

--- spec/smoke/jive_page/view_blog_page_spec.rb
+++ spec/smoke/jive_page/view_blog_page_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'View blog page' do
 
   it 'should return a 200 when requesting to view a blog' do
-    RestClient.get(ENV['base_url'] + '/blogs/hero-banner/2016/02/24/hero-banner-config',{:cookie => @authorisation, :content_type => 'application/json'}){|response|
+    RestClient.get(ENV['base_url'] + '/blogs/hero-banner',{:cookie => @authorisation, :content_type => 'application/json'}){|response|
       assert_code_and_body(response, 200)
     }
   end
