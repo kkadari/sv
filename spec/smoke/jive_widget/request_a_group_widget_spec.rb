8de8f9ff359d38636352feaf3f144f986d016edb
Tim Durden <tim.durden@surevine.com>
2016-11-03 15:09:57 +0000

    Removed check for group request app API in spec tests. No simple way to get querystring params. Group request checked elsewhere in suite.

--- spec/smoke/jive_widget/request_a_group_widget_spec.rb
+++ spec/smoke/jive_widget/request_a_group_widget_spec.rb
@@ -13,17 +13,4 @@ describe 'Request a group widget' do
     }
   end
 
-  it 'should return a 200 when posting details to /gadget/api/rpc ' do
-    payload = '[{"method":"gadgets.metadata","id":"gadgets.metadata","params":{"container":"default","ids":["http://localhost:8080/resources/grouprequestapp/app.xml"],"fields":["iframeUrls","modulePrefs.*","needsTokenRefresh","userPrefs.*","views.preferredHeight","views.preferredWidth","expireTimeMs","responseTimeMs","rpcServiceIds","tokenTTL"],"language":"en","country":"US","userId":"@viewer","groupId":"@self"}}]'
-
-    RestClient.post(ENV['base_url'] + '/gadgets/api/rpc?st=default%3AmiuZPNRf0H01UVrp3xgLp7dnR74rZIFqv7TPVAuG_wDECiSryEx3mUuEwTbh8hNwofQlyQ',payload,{:cookie => @authorisation,:content_type => 'application/json'}){|response|
-      fail('Failed with ' + response.code.to_s) if response.code != 200
-      @path = JSON.parse(response.body)[0]['result']['http://localhost:8080/resources/grouprequestapp/app.xml']['iframeUrls']['createview'].split('&st=',2)[1] # Gets necessary query string for posting to /gadget/api/rpc
-    }
-
-    RestClient.post(ENV['base_url'] + '/social/rpc?st=' + @path,payload,{:cookie => @authorisation,:content_type => 'application/json'}){|response|
-      assert_code_and_body(response, 200)
-    }
-  end
-
 end
